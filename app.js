7770aa54c262cbc9e1c8b70c0900138eeecb865f
Kayda Seutter <kmseutter@gmail.com>
2025-06-26 21:25:10 -0600

    fixed app.js session middleware error

--- app.js
+++ app.js
@@ -14,22 +14,23 @@ const app = express();
 app.use(express.urlencoded({ extended: true })); // For form data
 app.use(express.json());                         // For JSON data
 app.use(express.static(path.join(__dirname, 'public'))); // Serve static files
+//Session middleware (must come BEFORE anything using req.session)
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'your-secret',
+  resave: false,
+  saveUninitialized: false
+}));
 
 app.use((req, res, next) => {
-  //res.locals.user = req.session.userId ? { id: req.session.userId } : null;
-  res.locals.user = "Jerry"
-    next();
+  res.locals.user = req.session.userId ? { id: req.session.userId } : null;
+  //res.locals.user = "Jerry"
+  next();
 });
 
 // View Engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(session({
-  secret: process.env.SESSION_SECRET || 'your-secret',
-  resave: false,
-  saveUninitialized: false
-}));
 
 app.use(flash()); // Flash middleware
 
